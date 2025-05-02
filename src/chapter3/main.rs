c43a0aceaf4cb0ec992ec6e2b9407741e4a9e051
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-02 18:59:41 +0900

    【add】3.2 データ型の実装

--- src/chapter3/main.rs
+++ src/chapter3/main.rs
@@ -1,8 +1,12 @@
+use std::io;
+
 // 定数は必ず型を宣言する必要があり、宣言が漏れていると実行時にエラーとなる
 const MAX_POINTS: u32 = 100_000;
 
 fn main() {
     practice_3_1();
+    println!("-------------------------------------");
+    practice_3_2();
 }
 
 fn practice_3_1() {
@@ -35,5 +39,71 @@ fn practice_3_1() {
         // 同じ変数名で違う型に変換出来る
         let spaces = "   ";
         let spaces = spaces.len();
+    }
+}
+
+fn practice_3_2() {
+    // 数値演算
+    {
+        // 足し算
+        let sum = 5 + 10;
+        println!("The sum is: {}", sum);
+
+        // 引き算
+        let difference = 95.5 - 4.3;
+        println!("The difference is: {}", difference);
+
+        // 掛け算
+        let product = 4 * 30;
+        println!("The product is: {}", product);
+
+        // 割り算
+        let quotient = 56.7 / 32.2;
+        println!("The quotient is: {}", quotient);
+        // 結果は0
+        let floored = 2 / 3;
+        println!("The floored quotient is: {}", floored);
+
+        // 余り
+        let remainder = 43 % 5;
+        println!("The remainder is: {}", remainder);
+    }
+
+    // 複合型（タプル）
+    {
+        // 宣言と分解
+        let tup = (500, 6.4, 1);
+        let (x, y, z) = tup;
+        println!("The value of y is: {}", y);
+
+        // 添字アクセス
+        println!("{} {} {}", tup.0, tup.1, tup.2);
+    }
+
+    // 複合型（配列）
+    {
+        // サイズは固定長になる
+        // ベクタ型を使うことが多いかもしれないが、月のリストなどの固定の要素を用いる際は便利だ
+        let a = [1, 2, 3, 4, 5];
+
+        // let a = [3, 3, 3, 3, 3];と同じ
+        // let a = [3; 5];
+
+        println!("Please enter an array index.");
+
+        let mut index = String::new();
+
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to read line");
+
+        let index: usize = index
+            .trim()
+            .parse()
+            .expect("Index entered was not a number");
+
+        let element = a[index];
+
+        println!("The value of the element at index {} is: {}", index, element);
     }
 }
