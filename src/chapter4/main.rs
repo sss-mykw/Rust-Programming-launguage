6aa28112158cff15dc931357906a0db3e83389df
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-03 14:48:28 +0900

    【add】4.1 所有権とは？

--- src/chapter4/main.rs
+++ src/chapter4/main.rs
@@ -1,3 +1,47 @@
 fn main() {
-    println!("Hello, world!");
+    practice_4_1();
+}
+
+fn practice_4_1() {
+    // 変数とデータの相互作用法: ムーブ
+    {
+        let s1 = String::from("hello");
+        let s2 = s1;
+
+        // 下記はエラーになる。s1はs2にムーブされたのでs1は使用できない
+        // println!("{}, world!", s1);
+    }
+    
+    // 変数とデータの相互作用法: クローン
+    {
+        let s1 = String::from("hello");
+        // ヒープデータが実際にコピーさせる
+        let s2 = s1.clone();
+
+        println!("s1 = {}, s2 = {}", s1, s2);
+    }
+    
+    // スタックのみのデータ: コピー
+    {
+        let x = 5;
+        let y = x;
+
+        // スカラー型はcopy型である。
+        println!("x = {}, y = {}", x, y);
+    }
+    
+    // 所有権と関数
+    {
+        let s1 = String::from("hello");
+        let (s2, len) = calculate_length(s1);
+        
+        println!("The length of '{}' is {}.", s2, len);
+    }
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    // 参照を使っていないので冗長な書き方になっている
+    (s, length)
 }
