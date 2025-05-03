194f5c5c0499020b5140e0c1c188c9c4672042a9
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-03 14:28:51 +0900

    【add】3.5 制御フロー

--- src/chapter3/main.rs
+++ src/chapter3/main.rs
@@ -6,7 +6,9 @@ const MAX_POINTS: u32 = 100_000;
 fn main() {
     // practice_3_1();
     // practice_3_2();
-    practice_3_3();
+    // practice_3_3();
+    // 3_4は「コメント」なので特になし
+    practice_3_5();
 }
 
 fn practice_3_1() {
@@ -143,4 +145,53 @@ fn print_labeled_measurement(value: i32, unit_label: char) {
 fn plus_one(x: i32) -> i32 {
     // 同様にセミコロンを付けてしまうと、返り値の型定義と矛盾し、コンパイルエラーとなる
     x + 1
+}
+
+fn practice_3_5() {
+    // if式
+    {
+        let condition = true;
+        let number = if condition { 5 } else { 6 };
+        println!("The value of number is: {}", number);
+    }
+
+    // ループ
+    // loop
+    {
+        let mut count = 0;
+        // ラベルを用いた制御
+        'counting_up: loop {
+            println!("count = {}", count);
+            let mut remaining = 10;
+
+            loop {
+                println!("remaining = {}", remaining);
+                if remaining == 9 {
+                    // 内側のloopを終了させる
+                    break;
+                }
+                if count == 2 {
+                    // ラベルが付いた（外側の）ループを終了させる
+                    break 'counting_up;
+                }
+                remaining -= 1;
+            }
+
+            count += 1;
+        }
+        println!("End count = {}", count);
+    }
+
+    // for
+    {
+        let a = [10, 20, 30, 40, 50];
+        for element in a {
+            println!("The value is: {}", element);
+        }
+
+        for number in (1..4).rev() {
+            println!("{}!", number);
+        }
+        println!("LIFTOFF!!!");
+    }
 }
