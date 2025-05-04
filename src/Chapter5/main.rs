bfba4528ac13b6cc44a51b5034368e1f92da3717
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-04 14:06:24 +0900

    【add】5.2 構造体を使ったプログラム例 と 5.3 メソッド記法

--- src/Chapter5/main.rs
+++ src/Chapter5/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    practice_5_1();
-    
+    // practice_5_1();
+    practice_5_2_and_5_3();
 }
 
 fn practice_5_1() {
@@ -34,4 +34,63 @@ struct User {
     email: String,
     sign_in_count: u64,
     active: bool,
+}
+
+fn practice_5_2_and_5_3() {
+    // メソッドの定義やデバッグ情報の出力
+    {
+        let rectangle = Rectangle {
+            width: 30,
+            height: 50,
+        };
+        println!("The area of the rectangle is {} square pixels.", rectangle.area());
+        println!("{:?}", rectangle);
+        println!("{:#?}", rectangle);
+    }
+
+    // 引数の多いメソッド
+    {
+        let rect1 = Rectangle { width: 30, height: 50 };
+        let rect2 = Rectangle { width: 10, height: 40 };
+        let rect3 = Rectangle { width: 60, height: 45 };
+
+        println!("Can rect1 hold rect2 ? : {}", rect1.can_hold(&rect2));
+        println!("Can rect1 hold rect3 ? : {}", rect1.can_hold(&rect3));
+    }
+
+    // 関連関数
+    {
+        let square = Rectangle::square(30);
+        println!("The area of the square is {} square pixels.", square.area());
+        println!("{:?}", square);
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    // otherはreadしかしないので不変借用となる
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+// implを分けることも出来る
+impl Rectangle {
+    // 関連関数
+    // 引数にselfを取らないが、対象の構造体と関連がある場合に使用する
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
