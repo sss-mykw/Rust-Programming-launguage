622a6323a9b6150fbd550a66cfd8aec0a4b04aaa
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-04 13:54:21 +0900

    【add】5.1 構造体を定義し、インスタンス化する

--- src/Chapter5/main.rs
+++ src/Chapter5/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    practice_5_1();
+    
+}
+
+fn practice_5_1() {
+    let user1 = build_user(String::from("<EMAIL>"), String::from("someusername123"));
+    
+    // mutを付けるとフィールドが可変になる
+    let mut user2 = build_user(String::from("<EMAIL>"), String::from("someusername123"));
+    user2.email = String::from("<EMAIL>hoge");
+    
+    // 構造体更新記法
+    let user3 = User {
+        email: String::from("<EMAIL>"),
+        username: String::from("someusername123"),
+        ..user1
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    // フィールド初期化省略記法
+    // メソッドの引数とフィールド名が同一の場合に使用可能
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
