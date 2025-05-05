3cee4d4cff8fd5fba771c9e1d01e91f1dcb7c550
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-05 13:32:59 +0900

    【add】6.2 match制御フロー演算子

--- src/chapter6/main.rs
+++ src/chapter6/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    practice_6_1();
+    // practice_6_1();
+    practice_6_2();
 }
 
 fn practice_6_1() {
@@ -53,5 +54,73 @@ impl Message {
                 println!("ChangeColor");  
             }
         }
+    }
+}
+
+fn practice_6_2() {
+    {
+        let penny = Coin::Penny;
+        println!("{}", value_in_cents(penny))
+    }
+
+    // 値に束縛されるパターン
+    {
+        let alaska = UsState::Alaska;
+        let quarter = Coin::Quarter(alaska);
+        println!("{}", value_in_cents(quarter))   
+    }
+
+    // Option<T>とのマッチ
+    {
+        let five = Some(5);
+        let six = plus_one(five);
+        let none = plus_one(None);
+        println!("{:?}, {:?}", five, six);
+        println!("{:?}", none);   
+    }
+
+    // _というプレースホルダー
+    {
+        let some_u8_value = 0u8;
+        match some_u8_value {
+            1 => println!("one"),
+            3 => println!("three"),
+            5 => println!("five"),
+            7 => println!("seven"),
+            _ => (),
+        }
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+#[derive(Debug)] // すぐに州を点検できるように
+enum UsState {
+    Alabama,
+    Alaska,
+    // ... などなど
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
