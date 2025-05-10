ba143a8f1f5a2fa0e5773b5b9ceb8db9a554f603
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-11 00:38:11 +0900

    【add】9.1 panic!で回復不能なエラー

--- src/chapter9/main.rs
+++ src/chapter9/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    practice_9_1()
+}
+
+fn practice_9_1() {
+    let v = vec![1, 2, 3];
+
+    v[99];
+    
+    // 実行時に環境変数RUST_BACKTRACEをfullに指定すると詳細なトレースが表示される
 }
