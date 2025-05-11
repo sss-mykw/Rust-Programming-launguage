d38dc1470555147b5347efdce7da9a2d658cbb83
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-11 17:30:35 +0900

    【add】10.1 ジェネリックなデータ型

--- src/chapter10/main.rs
+++ src/chapter10/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    practice_10_1()
+}
+
+fn practice_10_1() {
+    // 構造体定義
+    {
+        let point = Point { x: 5, y: 10 };
+        let point = Point { x: 1.0, y: 4.0 };
+    }
+    
+    // enum定義
+    // OptionやResult型など
+    
+    // メソッド定義
+    {
+        let point = Point { x: 5, y: 10 };
+        println!("x: {}", point.x());
+    }
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl <T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
 }
