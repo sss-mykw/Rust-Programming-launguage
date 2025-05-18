269487de7842db5986eab93bf4cf1eb488025462
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-18 13:26:23 +0900

    【add】15.1 ヒープのデータを指すBox<T>を使用する

--- src/chapter15/main.rs
+++ src/chapter15/main.rs
@@ -1,3 +1,29 @@
+use crate::List::{Cons, Nil};
+
 fn main() {
-    println!("Hello, world!");
+    practice_15_1();
+}
+
+fn practice_15_1() {
+    // Box<T>を使ってヒープにデータを確保する
+    {
+        // Boxを使うことで値5はスタックではなくヒープに確保される
+        let b = Box::new(5);
+        println!("b = {}", b);
+        // スコープを抜ける際にスタックに格納されているBoxと、そのBoxが指し示すヒープに確保されている値5に対して実施される
+    }
+    
+    // ボックスで再帰的な型を可能にする
+    {
+        let list = Cons(1, Box::new(
+            Cons(2, Box::new(
+                Cons(3, Box::new(Nil))
+            ))
+        ));
+    }
+}
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
