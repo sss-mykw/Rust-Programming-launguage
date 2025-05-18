8b8b75b44f3e29baf2aa8cde5b5ef5fd7a87be8a
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-18 23:23:28 +0900

    【add】15.6 循環参照は、メモリをリークすることもある

--- src/chapter15/main.rs
+++ src/chapter15/main.rs
@@ -2,7 +2,7 @@ mod lib;
 
 use std::cell::RefCell;
 use std::ops::Deref;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use crate::List::{Cons, Nil};
 
 fn main() {
@@ -10,7 +10,8 @@ fn main() {
     // practice_15_2();
     // practice_15_3();
     // practice_15_4();
-    practice_15_5();
+    // practice_15_5();
+    practice_15_6();
 }
 
 fn practice_15_1() {
@@ -173,4 +174,62 @@ fn practice_15_5() {
         println!("b after = {:?}", b);
         println!("c after = {:?}", c);
     }
+}
+
+fn practice_15_6() {
+    // 循環参照を回避する: Rc<T>をWeak<T>に変換する
+    {
+        let leaf = Rc::new(Node {
+            value: 3,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![]),
+        });
+        // upgradeはWeak<T> → Option<Rc<T>> に変換する
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+        
+        // downgradeはRc<T> → Weak<T> に変換する。
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    }
+
+    // strong_countとweak_countへの変更を可視化する
+    {
+        let leaf = Rc::new(Node {
+            value: 3,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![]),
+        });
+
+        println!("leaf strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+
+        {
+            let branch = Rc::new(Node {
+                value: 5,
+                parent: RefCell::new(Weak::new()),
+                children: RefCell::new(vec![Rc::clone(&leaf)]),
+            });
+
+            *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+            println!("branch strong = {}, weak = {}", Rc::strong_count(&branch), Rc::weak_count(&branch));
+
+            println!("leaf strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+        }
+
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        println!("leaf strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
 }
