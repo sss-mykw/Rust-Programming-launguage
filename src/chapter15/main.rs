e3f96157c45e9c9635e84108539b5b2629fc5d4c
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-18 15:10:11 +0900

    【add】15.2 Derefトレイトでスマートポインタを普通の参照のように扱う

--- src/chapter15/main.rs
+++ src/chapter15/main.rs
@@ -1,7 +1,9 @@
+use std::ops::Deref;
 use crate::List::{Cons, Nil};
 
 fn main() {
-    practice_15_1();
+    // practice_15_1();
+    practice_15_2();
 }
 
 fn practice_15_1() {
@@ -26,4 +28,63 @@ fn practice_15_1() {
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+fn practice_15_2() {
+    // 参照外し演算子で値までポインタを追いかける
+    {
+        let x = 5;
+        let y = &x;
+        
+        assert_eq!(5, x);
+        // yはxへのポインタなので、参照が指している値を取得するには参照外しが必要になる
+        assert_eq!(5, *y);
+    }
+
+    // Box<T>を参照のように使う
+    {
+        let x = 5;
+        let y = Box::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+    
+    // 独自のスマートポインタを定義する
+    // Derefトレイトを実装して型を参照のように扱う
+    {
+        let x = 5;
+        let y = MyBox::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+
+    // 関数やメソッドで暗黙的な参照外し型強制
+    {
+        let m = MyBox::new(String::from("Rust"));
+        hello(&m);
+        // 参照外し型強制（Deref）がなかった場合は以下のような難しい記述になる
+        hello(&(*m)[..]);
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
