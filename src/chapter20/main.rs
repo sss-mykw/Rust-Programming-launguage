8051350cfc3e0df4fdd2c057002aacdad9b95efe
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-19 11:36:25 +0900

    【add】20.2 現在のサーバの実装で遅いリクエストをシミュレーションする

--- src/chapter20/main.rs
+++ src/chapter20/main.rs
@@ -1,8 +1,9 @@
 use fs::File;
 use std::fmt::format;
-use std::fs;
+use std::{fs, thread};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,8 +20,12 @@ fn handle_connection(mut stream: TcpStream) {
     
     // /に対応するリクエストかどうかを判定するための変数
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
