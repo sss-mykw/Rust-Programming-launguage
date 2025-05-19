f3fa256e67d75358c7ef0507920887d3b45e7444
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-19 20:11:28 +0900

    【add】ThreadPoolにDropトレイトを実装する

--- src/chapter20/mod.rs
+++ src/chapter20/mod.rs
@@ -48,9 +48,22 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}.", worker.id);
+            
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+    
+}
+
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -67,7 +80,7 @@ impl Worker {
         
         Worker {
             id,
-            thread,
+            thread: Some(thread),
         }
     }
 }
