e480437811db66e88323ec2c82c481d27a85185f
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-19 13:52:53 +0900

    【add】スレッドを格納するスペースを生成する

--- src/chapter20/mod.rs
+++ src/chapter20/mod.rs
@@ -1,10 +1,23 @@
-pub struct ThreadPool;
+use std::thread;
+use std::thread::Thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        ThreadPool
+        let mut threads = Vec::with_capacity(size);
+        
+        for _ in 0..size {
+            // TODO スレッドを生成してベクタを格納する
+        }
+        
+        ThreadPool {
+            threads
+        }
     }
 
     pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static {
